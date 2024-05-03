75a19afe209bbd6b1ee1d27a67288dd16d0c7c46
Giuseppe Buoncore <135731752+GiuseppeBuoncore@users.noreply.github.com>
2024-05-03 19:06:15 +0200

    Add files via upload

---
+++ Examples of Basic SQL queries task3.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE 'Titanic_dataset' ('PassengerId' TEXT,'Survived' TEXT,'Pclass' TEXT,'Name' TEXT,'Sex' TEXT,'Age' TEXT,'SibSp' TEXT,'Parch' TEXT,'Ticket' TEXT,'Fare' TEXT,'Cabin' TEXT,'Embarked' TEXT);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
